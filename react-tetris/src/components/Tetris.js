40fe51a8032c414b64cdbb57a7c8e1edc4a346b7
chuckan13 <ca9@princeton.edu>
2019-08-23 15:25:12 +0000

    sdf

--- react-tetris/src/components/Tetris.js
+++ react-tetris/src/components/Tetris.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { createStage, checkCollision } from '../gameHelpers';
 import { StyleTetrisWrapper, StyleTetris } from './styles/StyleTetris';
+import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
 
@@ -10,47 +11,56 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
-	const [ dropTime, setDropTime ] = useState(null);
-	const [ gameOver, setGameOver ] = useState(false);
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
 
-	const [ player, updatePlayerPos, resetPlayer, playerRotate ] = usePlayer();
-	const [ stage, setStage ] = useStage(player, resetPlayer);
+    const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
+    const [stage, setStage] = useStage(player, resetPlayer);
 
-	console.log('re-render');
+    console.log('re-render');
 
-	const movePlayer = dir => {
-		if (!checkCollision(player, stage, { x: dir, y: 0 })) {
-			updatePlayerPos({ x: dir, y: 0 });
-		}
-	};
+    const movePlayer = dir => {
+        if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+            updatePlayerPos({ x: dir, y: 0 });
+        }
+    };
 
-	const startGame = () => {
-		//reset everything
-		setStage(createStage());
-		resetPlayer();
-		setGameOver(false);
-	};
+    const startGame = () => {
+        //reset everything
+        setStage(createStage());
+        setDropTime(1000);
+        resetPlayer();
+        setGameOver(false);
+    };
 
-	const drop = () => {
-		if (!checkCollision(player, stage, { x: 0, y: 1 })) {
-			updatePlayerPos({ x: 0, y: 1, collided: false });
+    const drop = () => {
+        if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+            updatePlayerPos({ x: 0, y: 1, collided: false });
 
-		} else {
-			if (player.pos.y < 1) {
-				console.log('GAME OVER');
-				setGameOver(true);
-				setDropTime(null);
-			}
-			updatePlayerPos({ x: 0, y: 0, collided: true });	
-		}
-	};
+        } else {
+            if (player.pos.y < 1) {
+                console.log('GAME OVER');
+                setGameOver(true);
+                setDropTime(null);
+            }
+            updatePlayerPos({ x: 0, y: 0, collided: true });
+        }
+    };
 
-	const dropPlayer = () => {
-        // drop();
-        console.log(drop())
-	};
+    const keyUp = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 40) {
+                setDropTime(1000);
+            }
+        }
+    }
 
-	// const hardDropPlayer = () => {
+    const dropPlayer = () => {
+        setDropTime(null);
+        drop();
+    };
+
+    // const hardDropPlayer = () => {
     //     console.log(drop())
     //     console.log(drop())
     //     console.log(drop())
@@ -75,52 +85,56 @@ const Tetris = () => {
     //     try{drop()} catch{
     //         console.log("drop error")
     //     }
-	// 	// var i;
-	// 	// for (i = 0; i < 30; i++) {
+    // 	// var i;
+    // 	// for (i = 0; i < 30; i++) {
     //     //     console.log(drop())
-	// 	// 	// if (drop()) {
+    // 	// 	// if (drop()) {
     //     //     //     console.log('done dropping')
-	// 	// 	// 	break;
-	// 	// 	// }
-	// 	// }
-	// };
+    // 	// 	// 	break;
+    // 	// 	// }
+    // 	// }
+    // };
 
-	const move = ({ keyCode }) => {
-		if (!gameOver) {
-			if (keyCode === 37) {
-				movePlayer(-1);
-			} else if (keyCode === 39) {
-				movePlayer(1);
-			} else if (keyCode === 40) {
-				dropPlayer();
-			} else if (keyCode === 38) {
-				playerRotate(stage, 1);
-            } 
+    const move = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 37) {
+                movePlayer(-1);
+            } else if (keyCode === 39) {
+                movePlayer(1);
+            } else if (keyCode === 40) {
+                dropPlayer();
+            } else if (keyCode === 38) {
+                playerRotate(stage, 1);
+            }
             // else if (keyCode === 32) {
-			// 	hardDropPlayer();
-			// }
-		}
-	};
+            // 	hardDropPlayer();
+            // }
+        }
+    };
 
-	return (
-		<StyleTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
-			<StyleTetris>
-				<Stage stage={stage} />
-				<aside>
-					{gameOver ? (
-						<Display gameover={gameOver} text="Game Over" />
-					) : (
-						<div>
-							<Display text="Score" />
-							<Display text="Rows" />
-							<Display text="Level" />
-						</div>
-					)}
-					<StartButton callback={startGame} />
-				</aside>
-			</StyleTetris>
-		</StyleTetrisWrapper>
-	);
+    useInterval(() => {
+        drop();
+    }, dropTime)
+
+    return (
+        <StyleTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)} onKeyUp={keyUp}>
+            <StyleTetris>
+                <Stage stage={stage} />
+                <aside>
+                    {gameOver ? (
+                        <Display gameover={gameOver} text="Game Over" />
+                    ) : (
+                            <div>
+                                <Display text="Score" />
+                                <Display text="Rows" />
+                                <Display text="Level" />
+                            </div>
+                        )}
+                    <StartButton callback={startGame} />
+                </aside>
+            </StyleTetris>
+        </StyleTetrisWrapper>
+    );
 };
 
 export default Tetris;
--- react-tetris/src/hooks/useStage.js
+++ react-tetris/src/hooks/useStage.js
@@ -2,37 +2,52 @@ import { useState, useEffect } from 'react';
 import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
-	const [ stage, setStage ] = useState(createStage());
+    const [stage, setStage] = useState(createStage());
+    const [rowsCleared, setRowsCleared] = useState(0);
 
-	useEffect(
-		() => {
-			const updateStage = prevStage => {
-				// First flush the stage
-				const newStage = prevStage.map(row => row.map(cell => (cell[1] === 'clear' ? [ 0, 'clear' ] : cell)));
+    useEffect(
+        () => {
+            setRowsCleared(0);
 
-				// Then draw the tetromino
-				player.tetromino.forEach((row, y) => {
-					row.forEach((value, x) => {
-						if (value !== 0) {
-							newStage[y + player.pos.y][x + player.pos.x] = [
-								value,
-								`${player.collided ? 'merged' : 'clear'}`
-							];
-						}
-					});
-				});
-				// Then check if we collided
-				if (player.collided) {
-					resetPlayer();
-				}
+            const sweepRows = newStage =>
+                newStage.reduce((ack, row) => {
+                    if (row.findIndex(cell => cell[0] === 0) === -1) {
+                        setRowsCleared(prev => prev + 1);
+                        ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+                        return ack;
+                    }
+                    ack.push(row);
+                    return ack;
+                }, [])
 
-				return newStage;
-			};
+            const updateStage = prevStage => {
+                // First flush the stage
+                const newStage = prevStage.map(row => row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)));
 
-			setStage(prev => updateStage(prev));
-		},
-		[ player, resetPlayer ]
-	);
+                // Then draw the tetromino
+                player.tetromino.forEach((row, y) => {
+                    row.forEach((value, x) => {
+                        if (value !== 0) {
+                            newStage[y + player.pos.y][x + player.pos.x] = [
+                                value,
+                                `${player.collided ? 'merged' : 'clear'}`
+                            ];
+                        }
+                    });
+                });
+                // Then check if we collided
+                if (player.collided) {
+                    resetPlayer();
+                    return sweepRows(newStage);
+                }
 
-	return [ stage, setStage ];
+                return newStage;
+            };
+
+            setStage(prev => updateStage(prev));
+        },
+        [player, resetPlayer]
+    );
+
+    return [stage, setStage, rowsCleared];
 };
